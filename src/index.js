aa58535ce3007434623b794cae47ff3649efe7e3
Priti Parkhe <parkhepriti47@gmail.com>
2024-04-09 13:30:36 +0530

    mongoDB atlas connected

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import exprees from "express"
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("ERROR:", (error) => {console.log("ERROR: ", error);
+        throw error
+    })
+    app.listen(process.env.PORT, ()=>{
+        console.log(`App is listening on port ${process.env.PORT}`);
+    })
+    } catch (error) {
+        console.log("ERROR: ", error);
+        
+    }
+})()*/
