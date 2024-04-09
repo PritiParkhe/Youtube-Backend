cd55ee04bf485a3218a5fe40e71db183bafc1615
Priti Parkhe <parkhepriti47@gmail.com>
2024-04-09 17:25:28 +0530

    add User and Video model

---
+++ src/models/video.model.js
@@ -0,0 +1,46 @@
+import momgoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = new Schema(
+    {
+        videoFile:{
+            type: String, //cloudnery url
+            required: true
+        },
+        thumbnail:{
+            type: String, //cloudnery url
+            required: true
+        },
+        title:{
+            type: String, //cloudnery url
+            required: true
+        },
+        decription:{
+            type: String, //cloudnery url
+            required: true
+        },
+        duration:{
+            type: Number,
+            required: true
+        },
+        views:{
+            type: Number,
+            default: 0
+        },
+        isPublished:{
+            type: Boolean,
+            default: true
+        },
+        owner:{
+            type: Schema.Types.ObjectId,
+            ref:"User"
+        }
+
+
+        
+    },
+    {
+        timestamps: true
+    }
+)
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video = momgoose.model("Video", videoSchema)
