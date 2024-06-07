412a90993cbd7cbb2065dfae89314b362cca402d
Priti Parkhe <parkhepriti47@gmail.com>
2024-06-07 19:43:33 +0530

    changers in user controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -23,7 +23,8 @@ const registerUser = asyncHandler(async(req, res) => {
     throw new ApiError(400,"All fields are requied")
     
    }
-   const existedUser = User.findOne({
+   
+   const existedUser = await User.findOne({
     $or: [{ username },{ email }]
    })
    if (existedUser) {
