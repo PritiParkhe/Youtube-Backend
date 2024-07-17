42b7b659c41d6df30ee850dc492a7809388bee55
Priti Parkhe <parkhepriti47@gmail.com>
2024-07-17 10:49:47 +0530

    add comment, like, playlist models

---
+++ src/models/like.model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new Schema({
+  video : {
+    type : Schema.Types.ObjectId,
+    ref : "Video",
+  },
+  comment : {
+    type : Schema.Types.ObjectId,
+    ref : "Comment",
+  },
+  likedBy : {
+    type : Schema.Types.ObjectId,
+    ref : "User",
+  },
+
+},
+{ timestamps : true }
+);
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,4 @@
-import momgoose, {Schema} from "mongoose";
+import mongoose, {Schema} from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const videoSchema = new Schema(
     {
